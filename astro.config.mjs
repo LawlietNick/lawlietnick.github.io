520b5c2d6a8057f340b877d27e1d34dc05602960
Niko Karppinen <nicknoir@gmail.com>
2025-08-05 21:17:35 +0300

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://lawlietnick.github.io",
+  site: "https://karppinen.one",
   integrations: [mdx(), sitemap(), tailwind()],
 });
