8bc46a9985bf44d9c3446fc16723ad8824093948
Niko Karppinen <nicknoir@gmail.com>
2025-08-12 20:10:07 +0300

    Added copied to clipboard button for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { addCopyButton } from 'shiki-transformer-copy-button';
 
 export default defineConfig({
   site: "https://karppinen.one",
   integrations: [mdx(), sitemap(), tailwind()],
 
   markdown: {
+    syntaxHighlight: 'shiki',
     shikiConfig: {
       themes: {
         light: "dracula",
         dark: "dracula"
       },
       wrap: true,
+      transformers: [addCopyButton({ toggle: 5000 })]
     }
   },
 });
