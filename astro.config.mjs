bd4336202d448d1347be02796487538eb134f3de
Niko Karppinen <nicknoir@gmail.com>
2025-08-19 20:22:20 +0300

    Added Umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { addCopyButton } from 'shiki-transformer-copy-button';
+import umami from "@yeskunall/astro-umami";
 
 export default defineConfig({
   site: "https://karppinen.one",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    umami({ id: "94db1cb1-74f4-4a40-ad6c-962362670409" })
+  ],
 
   markdown: {
     syntaxHighlight: 'shiki',
